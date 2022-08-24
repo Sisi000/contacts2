b00235467083f463cbe719d6fd014a47f3845629
Sanja <sivansic@gmail.com>
2022-08-23 21:18:33 -0600

    Added for loops to add and delete

--- src/main/java/com/downloadwink/contacts/model/TestPrintContact.java
+++ src/main/java/com/downloadwink/contacts/model/TestPrintContact.java
@@ -8,40 +8,49 @@ public class TestPrintContact {
         ContactHome contacts = ContactHome.getInstance();
         BirthDay birthDate = BirthDay.getInstance();
 
-//        //All contacts
-//        List<Contact> allContacts = contacts.allContacts();
-//        System.out.println("All contacts: ");
-//        if (allContacts.size() > 0) {
-//            for (Contact e : allContacts) {
-//                System.out.println(e);
-//            }
-//        } else {
-//            System.out.println("No record found");
-//        }
+        //All contacts
+        List<Contact> allContacts = contacts.allContacts();
+        System.out.println("All contacts: ");
+        if (allContacts.size() > 0) {
+            for (Contact e : allContacts) {
+                System.out.println(e);
+            }
+        } else {
+            System.out.println("No record found");
+        }
 
 //        //Add contact
 //        Contact contact = new Contact("John", "Doe", "1234567890");
 //        contacts.addNewContact(contact);
 //        System.out.println("Added contact: " + contact);
 //        System.out.println("");
-//        System.out.println("New contact list: ");
-//        System.out.println(contacts.allContacts());
-
-
-//        //Delete contact
-//        System.out.println("Contact deleted");
-//        List<Contact> someContacts2 = contacts.findByLastName("Ivansic");
-//        if (someContacts2.size() > 0) {
-//            for (Contact contact2 : someContacts2) {
-//                contact2.remove(contacts);
-//                System.out.println(contact2);
-//                System.out.println("");
-//                System.out.println("New contact list: ");
-//                System.out.println(contacts.allContacts());
+//        if (contacts.allContacts().size() > 0) {
+//
+//            System.out.println("New contact list: ");
+//            for (Contact e : contacts.allContacts()) {
+//                System.out.println(e);
 //            }
 //        } else {
 //            System.out.println("No record found");
 //        }
+
+
+//        //Delete contact
+////        System.out.println("Contact deleted");
+////        List<Contact> someContacts2 = contacts.findByLastName("Ivansic");
+////        if (someContacts2.size() > 0) {
+////            for (Contact contact2 : someContacts2) {
+////                contact2.remove(contacts);
+////                System.out.println(contact2);
+////                System.out.println("");
+////                System.out.println("New contact list: ");
+////                for (Contact e : contacts.allContacts()) {
+////                    System.out.println(e);
+////                }
+////            }
+////        } else {
+////            System.out.println("No record found");
+////        }
 
         //Find by first name
 //        List<Contact> someContacts = contacts.findByFirstName("Siri");
